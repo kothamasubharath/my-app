11e7d6bf2322ecb76de4906823562825a46b031e
kothamasubharath <81374575+kothamasubharath@users.noreply.github.com>
2021-04-03 23:34:35 -0700

    delete v2

--- main/java/com/mycompany/app/App.java
+++ main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!!!!,v2.0" );
+      
           System.out.println( "Hello World!!!!!!,v4.0" );
     }
 }
